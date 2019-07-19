ab4bcc8afb00749e7a74428869156558ffbcdfbb
kokej <jamarqueza@gmail.com>
2019-07-19 20:45:56 +0200

    first component, nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'curso';
+export class AppComponent implements OnInit {
+  title = 'Curso de Angular';
+  newTitleOnInit = 'Breve introducción a Angular';
+  // newTitleInHtml = 'Introducción a Angular';
+
+  changeTitle(newTitle: string): string {
+    return (this.title = newTitle);
+  }
+
+  ngOnInit() {
+    // this.changeTitle(this.newTitleOnInit);
+  }
 }
